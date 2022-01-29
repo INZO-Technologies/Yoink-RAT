38c661870d57ba7757fd3042d3ab8fd29da08a02
yeah <61770151+zopac@users.noreply.github.com>
2022-01-28 21:10:16 -0600

    Update KonasAccounts.java

--- src/konas/installer/payload/impl/KonasAccounts.java
+++ src/konas/installer/payload/impl/KonasAccounts.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package konas.installer.payload.impl;
-
-/**
- *
- * @author ThnksCJ
- */
 
 import konas.installer.payload.Payload;
 import konas.installer.payload.Sender;
