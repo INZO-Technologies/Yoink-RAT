bd3779a5f2ca4807b36c7e367efa42b284586711
yeah <61770151+zopac@users.noreply.github.com>
2022-01-28 21:07:31 -0600

    Update TokenUtil.java

--- src/konas/installer/util/TokenUtil.java
+++ src/konas/installer/util/TokenUtil.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package konas.installer.util;
-
-/**
- *
- * @author ThnksCJ
- */
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
